1a0d39d7d241e84a7dee810f66ff1d937c26c00d
g-61235456-afk <g-61235456@moe-dl.edu.my>
2026-09-20 18:52:50 +0800

    Add Unit Masa Android activity

---
+++ app/src/main/java/my/edu/ppki/unitmasa/MainActivity.java
@@ -0,0 +1,20 @@
+package my.edu.ppki.unitmasa;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+ private WebView webView;
+ @Override public void onCreate(Bundle savedInstanceState){
+  super.onCreate(savedInstanceState);
+  webView=new WebView(this); setContentView(webView);
+  WebSettings s=webView.getSettings();
+  s.setJavaScriptEnabled(true); s.setDomStorageEnabled(true); s.setAllowFileAccess(true); s.setMediaPlaybackRequiresUserGesture(true); s.setTextZoom(100);
+  webView.setWebViewClient(new WebViewClient());
+  webView.loadUrl("file:///android_asset/index.html");
+ }
+ @Override public void onBackPressed(){ if(webView!=null&&webView.canGoBack()) webView.goBack(); else super.onBackPressed(); }
+}
